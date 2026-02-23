ebeee822af0256a04dbc49fa6772d27b5e946526
Chenguang Xu <xcg1234@gmail.com>
2026-02-23 11:23:43 -0500

    finished 7.1

---
+++ Assignments/Assignment7/Assignment7_1/Assignment7_1/MergeStringAlternatively.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment7_1
+{
+    internal class MergeStringAlternatively
+    {
+        public static string MergeAlternately(string word1, string word2)
+        {
+            StringBuilder merged = new StringBuilder();
+            int maxLength = Math.Max(word1.Length, word2.Length);
+            for (int i = 0; i < maxLength; i++)
+            {
+                if (i < word1.Length)
+                {
+                    merged.Append(word1[i]);
+                }
+                if (i < word2.Length)
+                {
+                    merged.Append(word2[i]);
+                }
+            }
+            return merged.ToString();
+        }
+    }
+}
---
+++ Assignments/Assignment7/Assignment7_1/Assignment7_1/SelectionSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment7_1
+{
+    internal class SelectionSort
+    {
+        public static void Sort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+    }
+}
