1a7459009d987223fa80fb00d4bdaaf00a3c6c5c
Chenguang Xu <xcg1234@gmail.com>
2026-02-24 16:11:42 -0500

    finished 7.3

---
+++ Assignments/Assignment7/Assignment7_3/Assignment7_3/FindNodeInBinaryTree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment7_3
+{
+    internal class FindNodeInBinaryTree
+    {
+        // Make TreeNode 'public' to fix CS0050 and CS0051
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val, TreeNode Left = null, TreeNode Right = null)
+            {
+                this.val = val;
+                this.left = Left;
+                this.right = Right;
+            }
+        }
+
+        public static TreeNode FindNode(TreeNode root, int target)
+        {
+            if (root == null) return null;
+            if (root.val == target) return root;
+            return FindNode(root.left, target) ?? FindNode(root.right, target);
+        }
+    }
+}
