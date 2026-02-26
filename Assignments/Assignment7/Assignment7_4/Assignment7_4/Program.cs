d656b8b5a43c38733989bdd3e07118ee8b2db4ac
Chenguang Xu <xcg1234@gmail.com>
2026-02-26 08:27:57 -0500

    finished 7.4

---
+++ Assignments/Assignment7/Assignment7_4/Assignment7_4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment7_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
