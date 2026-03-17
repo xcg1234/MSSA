c44ee578090019830d1eab67fa9452f0e00cf46b
Chenguang Xu <xcg1234@gmail.com>
2026-03-17 12:59:19 -0400

    finished 10.2

---
+++ Assignments/Assignment10/Assignment10_2/Assignment10_2/FindString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment10_2
+{
+    internal class FindString
+    {
+        //// find a string that starts and ends with a specific char
+        public static string FindStringWithChar(List<string> strings, char startChar, char endChar)
+        {
+            var result = from s in strings
+                         where s.StartsWith(startChar) && s.EndsWith(endChar)
+                         select s;
+            return result.FirstOrDefault();
+        }
+    }
+}
