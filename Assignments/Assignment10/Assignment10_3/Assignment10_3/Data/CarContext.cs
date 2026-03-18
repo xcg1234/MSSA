e1151653e4352db44a6f65bade68b348e77617f2
Chenguang Xu <xcg1234@gmail.com>
2026-03-18 15:53:42 -0400

    finished 10_3

---
+++ Assignments/Assignment10/Assignment10_3/Assignment10_3/Data/CarContext.cs
@@ -0,0 +1,29 @@
+﻿using Assignment10_3.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment10_3.Data
+{
+    internal class CarContext:DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=MSI;Initial Catalog=CarLogDatabase;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+        }
+
+        //some data seeding
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Car>().HasData(
+                new Car { VIN = "1HGCM82633A004352", Make = "Honda", Model = "Accord", Year = 2003 },
+                new Car { VIN = "1FAFP404X1F123456", Make = "Ford", Model = "Mustang", Year = 2001 },
+                new Car { VIN = "2T1BR32E54C123456", Make = "Toyota", Model = "Corolla", Year = 2004 }
+            );
+        }
+    }
+}
+
---
+++ Assignments/Assignment10/Assignment10_3/Assignment10_3/Models/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Assignment10_3.Models
+{
+    public class Car
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string VIN { get; set; }
+
+        [Required]
+        public string Make { get; set; }
+
+        [Required]
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+    }
+}
