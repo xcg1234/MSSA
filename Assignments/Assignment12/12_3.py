42745ab5448d0594f345bec578bc3de52ceffb7e
Chenguang Xu <xcg1234@gmail.com>
2026-04-06 09:00:58 -0400

    updates

---
+++ Assignments/Assignment12/12_3.py
@@ -0,0 +1,8 @@
+def removeDuplicates(s):
+        stack = []
+        for char in s:
+            if stack and stack[-1] == char:
+                stack.pop()
+            else:
+                stack.append(char)
+        return ''.join(stack)
